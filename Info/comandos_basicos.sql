cfae7b23fe6a85cabca93d149967f833da05f75c
Jose Reyes <josereyes.jdi@gmail.com>
2016-07-24 17:06:51 -0500

    comandos basicos

---
+++ Info/comandos_basicos.sql
@@ -0,0 +1,22 @@
+SELECT VERSION(); SELECT NOW();
+SELECT SIN(PI()/4), (4+1)*5;
+SELECT VERSION(), CURRENT_DATE;
+
+#Cancelar comando
+SELECT
+#    -> USER()
+#    -> \c
+
+# Prompt	Significado
+# mysql>	Listo para un nuevo comando.
+#    ->	Esperando la siguiente línea en un comando de múltiples líneas.
+
+# Limpiar pantalla
+!\ clear
+!\ cls
+system cls
+system clear
+
+CREATE DATABASE jojojo;
+USE jojojo
+SHOW TABLES;
