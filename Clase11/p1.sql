ef2beb2aa6ea8ef8b6afef4c1746554cc6dc73ae
Jose Reyes <josereyes.jdi@gmail.com>
2016-09-13 16:03:31 -0500

    practica 1 en sql

---
+++ Clase11/p1.sql
@@ -0,0 +1,18 @@
+1. Crear base de datos.
+2. Tabla llamada quiz
+3. Insertar XML: (a través de un archivo) (usando un insert)
+
+<universidad>
+<materia>
+<nombre> Cívica </nombre>
+<profesor> Manuel Amador </>
+</materia>
+
+</materia>
+<nombre> Frances </nombre>
+<profesor>Pepe Levou</profesor>
+</materia>
+</universidad>
+
+4. Select conteo total de materias
+5. Reemplazar 'Pepe Levou' con 'Zidane'
